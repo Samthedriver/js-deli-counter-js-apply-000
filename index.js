5ad6ebff71f627eaa7fb3d27934301e217342283
Samthedriver <amanuelabraham0202@gmail.com>
2018-07-22 21:06:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-function takeANumber(numberline, name) {
-  var katzDeliLine = [];
-  return numberline
+//var katzDeli = [];
+
+function takeANumber(currentLine, name) {
+  var katzDeli = currentLine;
+  if (currentLine.length == 0) {
+    var indx = 0;
+
+  }
+  else {
+    var indx = currentLine.length;
+
+  }
+
+  katzDeli[indx] = name;
+
+  var numberInLine = indx + 1;
+  var message = "Welcome, " + katzDeli[indx] + ". You are number " + numberInLine + " in line.";
+  return message
+}
+
+function nowServing(currentLine) {
+  if (currentLine === undefined || currentLine.length == 0) {
+    return "There is nobody waiting to be served!"
+  }
+  var firstPerson = currentLine[0];
+  currentLine.shift();
+  return "Currently serving " + firstPerson + "."
+}
+
+function currentLine(line) {
+
+  
 }
